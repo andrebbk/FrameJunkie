73e1baa3b8997a1790a3268703a725fd834ce07f
andrebbk <andre.f.m.silva@hotmail.com>
2023-10-14 12:38:09 +0100

    App main menu

---
+++ FrameJunkie/Controllers/Main/menu.js
@@ -0,0 +1,26 @@
+
+
+$('#btnHome').on('click', function (event){
+    console.log('Home button clicked');
+});
+
+$('#btnMovies').on('click', function (event){
+    console.log('Movies button clicked');
+});
+
+$('#btnNewMovie').on('click', function (event){
+    console.log('NewMovie button clicked');
+});
+
+$('#btnTvShows').on('click', function (event){
+    console.log('TvShows button clicked');
+});
+
+$('#btnNewTvShow').on('click', function (event){
+    console.log('NewTvShow button clicked');
+});
+
+$('#btnSettings').on('click', function (event){
+    console.log('Settings button clicked');
+    alert('settings');
+});
