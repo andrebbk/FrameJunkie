ffbe3cf9cb4a8f5c466883dacc8e984157678919
andrebbk <andre.f.m.silva@hotmail.com>
2023-10-14 12:53:29 +0100

    Improve menu flow

--- FrameJunkie/Controllers/Main/menu.js
+++ FrameJunkie/Controllers/Main/menu.js
@@ -2,25 +2,42 @@
 
 $('#btnHome').on('click', function (event){
     console.log('Home button clicked');
+
+    hideMenu();
 });
 
 $('#btnMovies').on('click', function (event){
     console.log('Movies button clicked');
+
+    hideMenu();
 });
 
 $('#btnNewMovie').on('click', function (event){
     console.log('NewMovie button clicked');
+
+    hideMenu();
 });
 
 $('#btnTvShows').on('click', function (event){
     console.log('TvShows button clicked');
+
+    hideMenu();
 });
 
 $('#btnNewTvShow').on('click', function (event){
     console.log('NewTvShow button clicked');
+
+    hideMenu();
 });
 
 $('#btnSettings').on('click', function (event){
     console.log('Settings button clicked');
     alert('settings');
+
+    hideMenu();
 });
+
+
+function hideMenu(){
+    $('#navcheck').click();
+}
