8eec72416b35b0089b6d00c3e8e9a06e5a4ede63
andrebbk <andre.f.m.silva@hotmail.com>
2024-03-03 18:25:49 +0000

    Improve home view interaction flow

--- FrameJunkie/Controllers/Main/menu.js
+++ FrameJunkie/Controllers/Main/menu.js
@@ -11,25 +11,43 @@ const { loadNewMovie } = require('./menu_newMovie.js');
 const { loadSettings } = require('./menu_settings.js'); 
 
 //INIT WITH HOME
-/*fs.readFile('./Views/Home/home.html', (err, data) => {
-    document.getElementById('content-main-app').innerHTML = data;
-    loadDashBoardData();
-});*/
-fs.readFile(path.join(__dirname, '../../Views/SettingsPages/settings.html'), (err, data) => {
-    document.getElementById('content-main-app').innerHTML = data;
-    loadSettings();
-    if(err != null) alert(err);
+fs.readFile('./Views/Home/home.html', (err, data) => {
+
+    setTimeout(() => {
+        document.getElementById('content-main-app').innerHTML += data;
+
+        loadDashBoardData();
+    
+        //Show data container
+        document.getElementById('loading_container').remove();
+        document.getElementById('home_container').style.visibility = "visible";
+    
+        $("#home_container").animate({"opacity": 1}, 600);
+    }, 1000);            
 });
 
  //BUTTONS
 $('#btnHome').on('click', function (event){
-    fs.readFile(path.join(__dirname, '../../Views/Home/home.html'), (err, data) => {
-        document.getElementById('content-main-app').innerHTML = data;
-        loadDashBoardData();
-		if(err != null) alert(err);
-     });
-
     hideMenu();
+    resetMenu();
+
+    fs.readFile(path.join(__dirname, '../../Views/Home/home.html'), (err, data) => {          
+		if(err != null) 
+            alert(err);
+        else{
+            setTimeout(() => {
+                document.getElementById('content-main-app').innerHTML += data;
+        
+                loadDashBoardData();
+            
+                //Show data container
+                document.getElementById('loading_container').remove();
+                document.getElementById('home_container').style.visibility = "visible";
+            
+                $("#home_container").animate({"opacity": 1}, 600);
+            }, 1000);   
+        }
+     });
 });
 
 $('#btnMovies').on('click', function (event){
@@ -76,5 +94,13 @@ function hideMenu(){
     closeToastMessage();
 
     $('#navcheck').trigger("click"); //.click();  deprecated
+}
+
+function resetMenu(){
+    var loadingHtml = '<div id="loading_container" class="loading-container">' +
+    '<img class="loading-img" src="./Content/Images/loading_animation.gif" width="60" height="60"></div>';
+
+    document.getElementById('content-main-app').innerHTML = loadingHtml;
+    document.getElementById("content-main-app").style.overflowY = "hidden";
 }
 
