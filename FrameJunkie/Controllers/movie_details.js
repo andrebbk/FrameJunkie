ed310404e3b47a5b2ecd100452ab8f72b6f57566
andrebbk <andre.f.m.silva@hotmail.com>
2024-03-25 19:57:28 +0000

    Edit movie pt2

--- FrameJunkie/Controllers/movie_details.js
+++ FrameJunkie/Controllers/movie_details.js
@@ -201,7 +201,7 @@ function addAndShowLoading(){
     $("#moviedetails_partial", "#movie-details-container").prepend($(loadingHTML).fadeIn('slow'));
 }
 
-function loadNewMovie() {   
+function loadEditMovie() {   
 
     //Load Filters
 	$('#movie-year', '#me-container').html('');
@@ -212,12 +212,12 @@ function loadNewMovie() {
 		$('#movie-year', '#me-container').append(optionHtml);
 	}
 
-	$('#movie-year').val(Number(crrYear)); //init Movie Year
+	$('#movie-year').val(Number(crrYear)); //init Movie Year     
 
     //BUTTONS SECTION
-    $('#btnSelectCover', '#me-container').on('click', function(){
+    $('#btnSelectCover', '.details-buttons-container').off('click').on('click', function(){
         $('#movie-cover-upload', '#me-container').trigger("click"); //.click() call is deprecated
-    });    
+    });  
 
     $('#movie-cover-upload', '#me-container').on('change', function(event){
         var reader = new FileReader();
@@ -252,13 +252,120 @@ function loadNewMovie() {
         newMovieCover = "";
     });    
 
+    //init numberstyle
+    $.fn.numberstyle = function(options) {
+	  
+		/*
+		 * Default settings
+		 */
+		var settings = $.extend({
+		  value: 0,
+		  step: undefined,
+		  min: undefined,
+		  max: undefined
+		}, options );
+	
+		/*
+		 * Init every element
+		 */
+		return this.each(function(i) {
+			
+		  /*
+		   * Base options
+		   */
+		  var input = $(this);
+			  
+		  /*
+		   * Add new DOM
+		   */
+          if(input.closest('.numberstyle-qty')[0] == undefined){
+                var container = document.createElement('div'),
+                btnAdd = document.createElement('div'),
+                btnRem = document.createElement('div'),
+                min = (settings.min) ? settings.min : input.attr('min'),
+                max = (settings.max) ? settings.max : input.attr('max'),
+                value = (settings.value) ? settings.value : parseFloat(input.val());
+
+                container.className = 'numberstyle-qty';
+                btnAdd.className = (max && value >= max ) ? 'qty-btn qty-add disabled' : 'qty-btn qty-add';
+                btnAdd.innerHTML = '+';
+                btnRem.className = (min && value <= min) ? 'qty-btn qty-rem disabled' : 'qty-btn qty-rem';
+                btnRem.innerHTML = '-';
+                input.wrap(container);
+                input.closest('.numberstyle-qty').prepend(btnRem).append(btnAdd);
+          }	 
+	
+		  /*
+		   * Attach events
+		   */
+		  // use .off() to prevent triggering twice
+		  $(document).off('click','.qty-btn').on('click','.qty-btn',function(e){
+			
+			var input = $(this).siblings('input'),
+				sibBtn = $(this).siblings('.qty-btn'),
+				step = (settings.step) ? parseFloat(settings.step) : parseFloat(input.attr('step')),
+				min = (settings.min) ? settings.min : ( input.attr('min') ) ? input.attr('min') : undefined,
+				max = (settings.max) ? settings.max : ( input.attr('max') ) ? input.attr('max') : undefined,
+				oldValue = parseFloat(input.val()),
+				newVal;
+			
+			//Add value
+			if ( $(this).hasClass('qty-add') ) {   
+			  
+			  newVal = (oldValue >= max) ? oldValue : oldValue + step,
+			  newVal = (newVal > max) ? max : newVal;
+			  
+			  if (newVal == max) {
+				$(this).addClass('disabled');
+			  }
+			  sibBtn.removeClass('disabled');
+			 
+			//Remove value
+			} else {
+			  
+			  newVal = (oldValue <= min) ? oldValue : oldValue - step,
+			  newVal = (newVal < min) ? min : newVal; 
+			  
+			  if (newVal == min) {
+				$(this).addClass('disabled');
+			  }
+			  sibBtn.removeClass('disabled');
+			  
+			}
+			  
+			//Update value
+			input.val(newVal).trigger('change');
+				
+		  });
+		  
+		  input.off('change').on('change',function(){
+			
+			const val = parseFloat(input.val()),
+				  min = (settings.min) ? settings.min : ( input.attr('min') ) ? input.attr('min') : undefined,
+				  max = (settings.max) ? settings.max : ( input.attr('max') ) ? input.attr('max') : undefined;
+			
+			if ( val > max ) {
+			  input.val(max);   
+			}
+			
+			if ( val < min ) {
+			  input.val(min);
+			}
+		  });
+		  
+		});
+	};	
+	  
+	//Init number controller
+	$('.numberstyle').numberstyle();
+
     //add star events
     const stars = document.querySelectorAll('#me-container .star');
     stars.forEach(el => el.addEventListener('click', event => {
         var btnRefresh = document.getElementById('btnRefreshMovieRating');
                 
         if(!document.getElementById('btnRefreshMovieRating').classList.contains("change"))
-        { document.querySelector('me-container .refresh-movie-rating').classList.toggle('change'); }      
+        { document.querySelector('#me-container .refresh-movie-rating').classList.toggle('change'); }      
     }));
 
     $('#btnRefreshMovieRating', '#me-container').on('click', function(){
@@ -277,23 +384,115 @@ function loadNewMovie() {
 
 //EDIT MOVIE
 $('#btnEditMovie', '#movie_detail_container').on('click', function(event){
-
-    $("#movie-details-container .details-buttons-container").animate({"opacity": 0 }, 200);
+    $("#movie-details-container .details-buttons-container").animate({"opacity": 0 }, 0);
     document.querySelector('#movie-details-container #moviedetails_partial #md-container').style.visibility = "collapse";    
     document.querySelector('#movie-details-container #moviedetails_partial #md-container').style.height = 0;    
-    $("#movie-details-container #moviedetails_partial #md-container").animate({"opacity": 0 }, 200);
+    $("#movie-details-container #moviedetails_partial #md-container").animate({"opacity": 0 }, 0);
 
     //Show loading
     addAndShowLoading();
-    loadNewMovie();
+
+    loadEditMovie();
     
     setTimeout(() => {
-         //Show data container
-         document.querySelector('#movie-details-container #moviedetails_partial #loading_container').remove();
+        //Show data container
+        document.querySelector('#movie-details-container #moviedetails_partial #loading_container').remove();
 
         document.querySelector('#movie-details-container #moviedetails_partial #me-container').style.visibility = "visible";   
         document.querySelector('#movie-details-container #moviedetails_partial #me-container').style.height = 530;   
         $("#movie-details-container #moviedetails_partial #me-container").animate({"opacity": 1 }, 600);
+
+        showEditButtons(true);
+        
     }, 500);
     
 });
+
+//BACK
+$('#btnBack', '#movie_detail_container').on('click', function(event){
+    $("#movie-details-container .details-buttons-container").animate({"opacity": 0 }, 0);
+    document.querySelector('#movie-details-container #moviedetails_partial #me-container').style.visibility = "collapse";    
+    document.querySelector('#movie-details-container #moviedetails_partial #me-container').style.height = 0;    
+    $("#movie-details-container #moviedetails_partial #me-container").animate({"opacity": 0 }, 0);
+
+    //Show loading
+    addAndShowLoading();
+
+    setTimeout(async () => {         
+        await loadMovieDetails();
+
+        //Show data container
+        document.querySelector('#movie-details-container #moviedetails_partial #loading_container').remove();
+
+        document.querySelector('#movie-details-container #moviedetails_partial #md-container').style.height = 530;  
+        document.querySelector('#movie-details-container #moviedetails_partial #md-container').style.visibility = "visible";    
+        $("#movie-details-container #moviedetails_partial #md-container").animate({"opacity": 1}, 600);
+
+        showEditButtons(false);   
+    }, 1000); 
+});
+
+function showEditButtons(showEditButtons){
+    if(showEditButtons === true){
+        document.querySelector("#movie-details-container #btnAddMovieView").style.display = "none";  
+        document.querySelector("#movie-details-container #btnBack").style.display = "";  
+        document.querySelector("#movie-details-container #btnEditMovie").style.display = "";  
+        document.querySelector("#movie-details-container #btnDeleteMovie").style.display = "none";  
+        document.querySelector("#movie-details-container #btnSelectCover").style.display = "";  
+    }
+    else{
+        document.querySelector("#movie-details-container #btnAddMovieView").style.display = "";  
+        document.querySelector("#movie-details-container #btnBack").style.display = "none";  
+        document.querySelector("#movie-details-container #btnEditMovie").style.display = "";  
+        document.querySelector("#movie-details-container #btnDeleteMovie").style.display = "";  
+        document.querySelector("#movie-details-container #btnSelectCover").style.display = "none";  
+    }
+
+    $("#movie-details-container .details-buttons-container").animate({"opacity": 1 }, 200);
+}
+
+async function deleteMovie(){
+    let output = true;
+
+    try
+    {
+        //delete movie
+        let deletedMovie = await knex('Movies')
+        .where({ MovieId: loadedMovieId})
+        .update({ 
+            Deleted: 1
+        }, ['MovieId', 'Deleted'])
+        .then(function(resp) {                    
+            output = true;
+            return resp;
+        }).catch(err => {
+            logger.error(err);
+            console.log(err);
+        });
+
+        if(deletedMovie.length > 0) {
+
+            //delete movie cover            
+            let deletedMovie = await knex('MovieCovers')
+            .where({ MovieId: loadedMovieId})
+            .update({ 
+                Deleted: 1
+            }, ['MovieId', 'Deleted'])
+            .then(function(resp) {                    
+                output = true;
+                return resp;
+            }).catch(err => {
+                logger.error(err);
+                console.log(err);
+            });                
+        }
+    }
+    catch(error){
+        logger.error(error);
+        console.error(error);
+        return false;
+    }  
+
+    return output;
+}
+
